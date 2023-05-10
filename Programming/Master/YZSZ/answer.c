cc860be02016becf26c778e8071b365a746aa98d
ZustFancake <ZustFancake@dimigo.hs.kr>
2023-05-10 18:47:49 +0900

    Update answer.c

--- Programming/Master/YZSZ/answer.c
+++ Programming/Master/YZSZ/answer.c
@@ -24,32 +24,46 @@ int main(){
 		
 		scanf("%*c");
 		
-		cnt = 0; int stt = 1;
-		int sc = 0, st = 0;
-		
-		while(1){
-			char k; scanf("%c", &k);
-			if (k == 'e') break;
-			if (k != '\n' && (k == '-' || k == '&')){
-				cnt++;
-				if  (k == '-') sc++;
-				else           st++;
-			}
-			else {
-				if (cnt != L){
-					if (sc == straight && st == tangled){
-						stt = 1;
-					}
-					else {
-						if  ((straight == 0 && st == tangled) || (sc == straight && tangled == 0) || ((straight == 0 && tangled == 0))) stt = 1;
-						else                                                                                                            stt = 0;
-					}
+		if (row != 0){ // 출력 결과가 -1이 아닌 경우 
+			
+			cnt = 0; int stt = 1;
+			int sc = 0, st = 0;
+			
+			while(1){
+				char k; scanf("%c", &k);
+				if (k == 'e') break;
+				if (k != '\n' && (k == '-' || k == '&')){
+					cnt++;
+					if  (k == '-') sc++;
+					else           st++;
 				}
-				cnt = 0; sc = 0; st = 0;
+				else {
+					if (cnt != L){
+						if (sc == straight && st == tangled){
+							stt = 1;
+						}
+						else {
+							if  ((straight == 0 && st == tangled) || (sc == straight && tangled == 0) || ((straight == 0 && tangled == 0))) stt = 1;
+							else                                                                                                            stt = 0;
+						}
+					}
+					cnt = 0; sc = 0; st = 0;
+				}
+			}
+			if (stt == 1) score++;
+		}
+		else{ // 출력 결과가 -1이여야 하는 경우 
+			int n; scanf("%d", &n);
+			if (n == -1) score++;
+			else {
+				while(1){
+					char k; scanf("%c", &k);
+					if (k == 'e') break;
+				}
 			}
 		}
-		if (stt == 1) score++;
 	}
+	
 	if      (score == T)             printf("정답입니다.");
 	else if (0 < score && score < T) printf("오답입니다. (통과하지 못한 테스트케이스가 있습니다.)");
 	else                             printf("오답입니다.");
